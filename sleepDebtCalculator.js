a08f417ad9f370b829544d2de7659452a9b8df3b
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-11 19:24:59 -0400

    refactored code

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,29 +1,36 @@
 function getSleepHours(day) {
-  if (day === 'monday') {
+  if (day === "monday") {
     return 8;
-  } else if (day === 'tuesday') {
+  } else if (day === "tuesday") {
     return 7;
-  } else if (day === 'wednesday') {
+  } else if (day === "wednesday") {
     return 6;
-  } else if (day === 'thursday') {
+  } else if (day === "thursday") {
     return 8;
-  } else if (day === 'friday') {
+  } else if (day === "friday") {
     return 5;
-  } else if (day === 'saturday') {
+  } else if (day === "saturday") {
     return 9;
-  } else if (day === 'sunday') {
+  } else if (day === "sunday") {
     return 7;
   }
-};
+}
 
-const getActualSleepHours = () => getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday');
+const getActualSleepHours = () =>
+  getSleepHours("monday") +
+  getSleepHours("tuesday") +
+  getSleepHours("wednesday") +
+  getSleepHours("thursday") +
+  getSleepHours("friday") +
+  getSleepHours("saturday") +
+  getSleepHours("sunday");
 
 function getIdealSleepHours() {
   const idealHours = 8;
   return idealHours * 7;
-};
+}
 
 const calculateSleepDebt = () => {
-    const actualSleepHours = getActualSleepHours();
-    const idealSleepHours = getIdealSleepHours();
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours();
 };
