830db3aba0c569571acaed19ecbb9c7372789cf9
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-11 19:13:37 -0400

    added in my sleep schdule to our sleepDebtCalculator

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,3 +1,17 @@
 function getSleepHours(day) {
-
-}
+  if (day === 'monday') {
+    return 8;
+  } else if (day === 'tuesday') {
+    return 7;
+  } else if (day === 'wednesday') {
+    return 6;
+  } else if (day === 'thursday') {
+    return 8;
+  } else if (day === 'friday') {
+    return 5;
+  } else if (day === 'saturday') {
+    return 9;
+  } else if (day === 'sunday') {
+    return 7;
+  }
+};
